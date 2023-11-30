e25742bebf5d43f39064c124524749c26566f014
meekdee <drewaju.16@gmail.com>
2023-11-30 13:21:00 -0800

    Not Done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Do Nothing, relax and Exit, You don try"""
+
+    import sys
+
+    argv = sys.argv[1:]
+    len = len(argv)
+    x = "argument" if len == 1 else "arguments"
+
+
