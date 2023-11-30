a18ef8de8887f225c9fce0c71d32db7063eccb5f
meekdee <drewaju.16@gmail.com>
2023-11-30 12:59:31 -0800

    Done 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
 
     result_add = add(a, b)
     print("{0} + {1} = {2}".format(a, b, result_add))
-    
+
     result_sub = sub(a, b)
     print("{0} - {1} = {2}".format(a, b, result_sub))
-    
+
     result_mul = mul(a, b)
     print("{0} * {1} = {2}".format(a, b, result_mul))
-    
+
     result_div = div(a, b)
     print("{0} / {1} = {2}".format(a, b, result_div))
