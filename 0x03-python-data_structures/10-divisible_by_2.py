d85d96ea334edf4d44480c88d2805e24cb6570d9
meekdee <drewaju.16@gmail.com>
2023-12-04 04:28:11 -0800

    Done 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    list_result = []
+    for i in range(len(my_list)):
+        list_result.append(my_list[i] % 2 == 0)
+
+    return list_result
